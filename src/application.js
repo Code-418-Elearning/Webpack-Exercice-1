21b5e84e410d8a4147d61986852f0109f2357c70
Trystan <trystansarrade@gmail.com>
2021-02-09 15:35:52 +0100

    Création de l'exercice

---
+++ src/application.js
@@ -0,0 +1,11 @@
+/** 
+ * Ceci est le point d'entré de notre application NodeJS 
+ * En soit ce script ne fait rien d'autre que d'écrire "Hello World" dans la console.
+ * */
+
+
+function Hello(text){
+    console.log('Hello ' + text)
+}
+
+Hello('World')
